4c936de3bf58dbd0ee9de2789d8107549048e565
samudjgn <samudjgn@gmail.com>
2026-09-12 23:01:36 -0500

    finalizacion de clase conexionDB

--- src/main/java/Database/ConexionDB.java
+++ src/main/java/Database/ConexionDB.java
@@ -1,5 +1,26 @@
 package TandaPenaltis;
 
 public class ConexionDB {
-    
+
+    private static final String URL = "jdbc:mysql://localhost:3306/torneo_penaltis";
+    private static final String USER = "samu_db";
+    private static final String PASSWORD = "s4mu1t0123321";
+
+    public static Connection obtenerConexion() {
+        Connection conexion = null;
+        try {
+            // Invoca al Driver que inyectaste en el pom.xml de Maven
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establece el puente físico con el servidor
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("¡Conexión exitosa a la base de datos de tu torneo!");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error crítico: No se encontró el driver JDBC de MySQL.");
+        } catch (SQLException e) {
+            System.out.println("Fallo de conexión a la base de datos: " + e.getMessage());
+        }
+        return conexion;
+    }
 }
